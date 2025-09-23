0b96c0452e991702637b1c6592f370493c586029
Kimchanghee <k931103@gmail.com>
2025-09-24 03:33:52 +0900

    Create Settings.js

---
+++ src/pages/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <div className="page-container">
+      <h2>설정</h2>
+      <p>설정 기능은 개발 중입니다.</p>
+    </div>
+  );
+};
+
+export default Settings;
