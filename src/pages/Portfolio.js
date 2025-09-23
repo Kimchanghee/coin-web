c2c42c3539084560b65db333aa74dbdda4194f1d
Kimchanghee <k931103@gmail.com>
2025-09-24 03:33:42 +0900

    Create Portfolio.js

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <div className="page-container">
+      <h2>포트폴리오</h2>
+      <p>포트폴리오 기능은 개발 중입니다.</p>
+    </div>
+  );
+};
+
+export default Portfolio;
